cea4402b1a3d37c63a98e0c86475a3fd812b1845
Mohammadtaha1122 <moammadtaha686@gmail.com>
2025-09-05 21:43:02 +0330

    v5

--- src/components/wrapperNavbar.jsx
+++ src/components/wrapperNavbar.jsx
@@ -3,7 +3,7 @@ import Navbar from "./navbar";
 
 export default function WrapperNavbar() {
   return (
-    <Suspense fallback={<div>Loading search...</div>}>
+    <Suspense fallback={<Navbar />}>
       <Navbar />
     </Suspense>
   );
